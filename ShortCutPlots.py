13afe131181cba6b2dd010bb3f1f03e5c34e2ec4
Nataliia Bagan <bagannatasha@gmail.com>
2024-05-28 11:52:26 +0200

    AUC Plots

--- ShortCutPlots.py
+++ ShortCutPlots.py
@@ -12,9 +12,9 @@ def plot(
     Plots results, achieved from conducted experiment using results.npy file.
 
     This function creates plots of 3 experiments both for deterministic and windy environments. The first experiment shows
-    RMS error for each considered td_steps value and alpha value. It is done for each of the 3 agents: Q-Learning, SARSA and
+    AUC for each considered td_steps value and alpha value. It is done for each of the 3 agents: Q-Learning, SARSA and
     Expected SARSA; and for each of the 2 environments. The second experiment plots learning curves of the 3 agents for considered
-    td_steps values and best alpha for each of them (based on RMS error). The last experiment takes best values of td_steps and
+    td_steps values and best alpha for each of them (based on AUC). The last experiment takes best values of td_steps and
     alpha for each agent and plots learning curves for them.
 
     :param smoothing_window: size of the smoothing window
@@ -37,28 +37,28 @@ def plot(
         best_alphas_SARSA_dict = {}
         best_alphas_ExpSARSA_dict = {}
 
-        ### EXPERIMENT 1###
+        ### EXPERIMENT 1 ###
         # Plots comparing average RMS errors based on td_steps(i) and alpha(j) for each agent
         QLearn_plot = ComparisonPlot(title="Average RMS errors of Q-learning based on td_steps and alpha.")
         SARSA_plot = ComparisonPlot(title="Average RMS errors of SARSA based on td_steps and alpha.")
         ExpSARSA_plot = ComparisonPlot(title="Average RMS errors of Expected SARSA based on td_steps and alpha.")
         for i in range(len(td_steps)):
-            QLearn_rms = np.zeros(len(alphas))
-            SARSA_rms = np.zeros(len(alphas))
-            ExpSARSA_rms = np.zeros(len(alphas))
+            QLearn_auc = np.zeros(len(alphas))
+            SARSA_auc = np.zeros(len(alphas))
+            ExpSARSA_auc = np.zeros(len(alphas))
             for j in range(len(alphas)):
-                # Average RMS error of a run of each agent, based on td_steps and alpha
-                QLearn_rms[j] = np.sqrt(np.mean((results[0, i, j, env] - optimal_val) ** 2))
-                SARSA_rms[j] = np.sqrt(np.mean((results[1, i, j, env] - optimal_val) ** 2))
-                ExpSARSA_rms[j] = np.sqrt(np.mean((results[2, i, j, env] - optimal_val) ** 2))
+                # AUC of a curve of each agent, based on td_steps and alpha
+                QLearn_auc[j] = np.trapz(results[0, i, j, env])
+                SARSA_auc[j] = np.trapz(results[1, i, j, env])
+                ExpSARSA_auc[j] = np.trapz(results[2, i, j, env])
             # Saving indexes of best alphas for each td_steps based on the environment for later learning curves
-            best_alphas_QLearn_dict[td_steps[i]] = np.argmin(QLearn_rms)
-            best_alphas_SARSA_dict[td_steps[i]] = np.argmin(SARSA_rms)
-            best_alphas_ExpSARSA_dict[td_steps[i]] = np.argmin(ExpSARSA_rms)
+            best_alphas_QLearn_dict[td_steps[i]] = np.argmax(QLearn_auc)
+            best_alphas_SARSA_dict[td_steps[i]] = np.argmax(SARSA_auc)
+            best_alphas_ExpSARSA_dict[td_steps[i]] = np.argmax(ExpSARSA_auc)
             # Adding comparison curves
-            QLearn_plot.add_curve(alphas, QLearn_rms, f"n={td_steps[i]}")
-            SARSA_plot.add_curve(alphas, SARSA_rms, f"n={td_steps[i]}")
-            ExpSARSA_plot.add_curve(alphas, ExpSARSA_rms, f"n={td_steps[i]}")
+            QLearn_plot.add_curve(alphas, QLearn_auc, f"n={td_steps[i]}")
+            SARSA_plot.add_curve(alphas, SARSA_auc, f"n={td_steps[i]}")
+            ExpSARSA_plot.add_curve(alphas, ExpSARSA_auc, f"n={td_steps[i]}")
         if env == 0:
             QLearn_plot.save("Q-Learning_comparison.png")
             SARSA_plot.save("SARSA_comparison.png")
@@ -74,15 +74,15 @@ def plot(
         SARSA_plot = LearningCurvePlot(title="SARSA learning curves based on td_steps hyperparam.")
         ExpSARSA_plot = LearningCurvePlot(title="Expected SARSA learning curves based on td_steps hyperparam.")
         for i in range(len(td_steps)):
-            # Experiment 1 - Q-learning agent
+            # Q-learning agent
             curve = results[0, i, best_alphas_QLearn_dict[td_steps[i]], env]
             QLearn_plot.add_curve(smooth(curve, smoothing_window), f"n={td_steps[i]} (alpha = {alphas[best_alphas_QLearn_dict[td_steps[i]]]})")
 
-            # Experiment 2 - SARSA agent
+            # SARSA agent
             curve = results[1, i, best_alphas_SARSA_dict[td_steps[i]], env]
             SARSA_plot.add_curve(smooth(curve, smoothing_window), f"n={td_steps[i]} (alpha = {alphas[best_alphas_SARSA_dict[td_steps[i]]]})")
 
-            # Experiment 3 - Expected SARSA agent
+            # Expected SARSA agent
             curve = results[2, i, best_alphas_ExpSARSA_dict[td_steps[i]], env]
             ExpSARSA_plot.add_curve(smooth(curve, smoothing_window), f"n={td_steps[i]} (alpha = {alphas[best_alphas_ExpSARSA_dict[td_steps[i]]]})")
         if env == 0:
@@ -106,7 +106,7 @@ def plot(
         curve = results[1, n, best_alphas_SARSA_dict[td_steps[n]], env]
         comparison_plot.add_curve(smooth(curve, smoothing_window), f"{algs[1]} (n={td_steps[n]}, alpha={alphas[best_alphas_SARSA_dict[td_steps[n]]]})")
         # Expected SARSA agent
-        n = 1 if env==0 else 0
+        n = 0
         curve = results[2, n, best_alphas_ExpSARSA_dict[td_steps[n]], env]
         comparison_plot.add_curve(smooth(curve, smoothing_window), f"{algs[2]} (n={td_steps[n]}, alpha={alphas[best_alphas_ExpSARSA_dict[td_steps[n]]]})")
         if env==0:
